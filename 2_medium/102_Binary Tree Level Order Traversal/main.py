16cc7b8fced36bb3c89683118b8eab5a74b8f170
pdhealy <pdlhealy@gmail.com>
2026-05-29 02:00:23 +0000

    Implement binary tree level order traversal with initial setup and examples
    - Added TreeNode class definition for binary tree nodes.
    - Implemented Solution class with levelOrder method stub.
    - Created main.py to demonstrate level order traversal with example trees.

---
+++ 2_medium/102_Binary Tree Level Order Traversal/main.py
@@ -0,0 +1,26 @@
+from logging import root
+
+from solution import Solution, TreeNode
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    # Example 1:
+
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    print(f"Example 1: {solution.levelOrder(root)}")
+
+
+    # Example 2:
+
+    root = TreeNode(1)
+    print(f"Example 2: {solution.levelOrder(root)}")
+
+    # Example 3:
+
+    root = None
+    print(f"Example 3: {solution.levelOrder(root)}")
